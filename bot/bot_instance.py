bc758b976c8daf4ccaa0d72a23596cd8319dafd1
Dmitriy <dimailyushen@gmail.com>
2024-09-03 02:22:11 +0300

    initial strucuture

---
+++ bot/bot_instance.py
@@ -0,0 +1,16 @@
+
+import os
+from os import getenv
+
+from aiogram import Bot, types
+from aiogram.client.default import DefaultBotProperties
+from dotenv import load_dotenv
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+
+default_properties = DefaultBotProperties(parse_mode="HTML")
+
+bot = Bot(
+    token=TOKEN,
+    default=default_properties
+)
