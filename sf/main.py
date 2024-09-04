6ef44628739ec686f69d3438009b1dc89134bfee
instarlin <instarlin@gmail.com>
2024-09-04 18:06:03 +0300

    modified:   bot/main.py 	modified:   requirements.txt 	new file:   sf/main.py

---
+++ sf/main.py
@@ -0,0 +1,48 @@
+from simple_salesforce import Salesforce, SalesforceMalformedRequest
+import os
+
+TOKEN = os.getenv("SF_TOKEN")
+USERNAME = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASSWORD")
+SECUTITY_TOKEN = os.getenv("SECUTITY_TOKEN")
+
+# sf = Salesforce(instance_url=INSTANCE, session_id=TOKEN)
+sf = Salesforce(username=USERNAME, password=PASSWORD, security_token=SECUTITY_TOKEN)
+
+# new_account_data = {
+#     'Name': 'New Account Name',
+#     'Phone': '1234567890',
+#     'Website': 'https://newaccount.com',
+#     'BillingStreet': '123 Main St',
+#     'BillingCity': 'San Francisco',
+#     'BillingState': 'CA',
+#     'BillingPostalCode': '94105',
+#     'BillingCountry': 'USA'
+# }
+
+# try:
+#     new_account = sf.Account.create(new_account_data)
+#     print(f"New account created with ID: {new_account['id']}")
+# except SalesforceMalformedRequest as e:
+#     print(f"Failed to create account: {e.content}")
+
+
+# Second Example
+query = "SELECT Id, Name FROM Account LIMIT 50"
+accounts = sf.query_all(query)
+
+for account in accounts['records']:
+    print(account['Id'], account['Name'])
+
+# new_account = sf.Account.create({
+#     'Name': 'New Account Name',
+#     'Phone': '1234567890'
+# })
+
+# print("New account created with ID:", new_account['id'])
+
+# sf.Account.update(new_account['id'], {
+#     'Phone': '0987654321'
+# })
+
+# print("Account updated successfully.")
